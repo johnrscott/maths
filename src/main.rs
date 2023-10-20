7416d503c807de0fe9c21f3fbb6fffe6b817905e
John Scott <johnrscott0@gmail.com>
2023-10-20 23:56:56 +0100

    Added some printing for variables and predicates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
+use std::fmt;
+
 enum Term {
     Variable(Variable),
     Function(Function),
 }
 
+impl fmt::Display for Term {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+	match self {
+	    Self::Variable(x) => write!(f, "{x}"),
+	    Self::Function(f) => unimplemented!(),
+	}
+    }
+}
+
+
 /// Primitive item that will be reasoned about
 struct Variable {
     name: String,
+}
+
+impl Variable {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+impl fmt::Display for Variable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+
+macro_rules! var {
+    ($name:expr) => {
+        Term::Variable(Variable::new(format!($name)))
+    };
 }
 
 /// Well-formed formula
@@ -13,21 +44,41 @@ enum Formula {
     Predicate(Predicate),
     Equal(Term, Term), // Only if equality is part of the logic
     Not(Box<Formula>),
-    BinaryConnective()
+    BinaryConnective(Box<Formula>, Box<Formula>),
 }
 
 /// Relation among variables which can be true or false
 struct Predicate {
-    arity: usize,
     name: String,
-    binding: Vec<Term>,
+    args: Vec<Term>,
+}
+
+impl Predicate {
+    fn new(name: String, args: Vec<Term>) -> Self {
+        Self { name, args }
+    }
+}
+
+impl fmt::Display for Predicate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}(", self.name)?;
+	for arg in &self.args {
+	    write!(f, "{arg},")?;
+	}
+	write!(f, ")")
+    }
+}
+
+macro_rules! pred {
+    ($name:expr, $($args:expr),*) => {
+        Predicate::new(format!($name), vec![$($args),*])
+    };
 }
 
 /// Rule for obtaining a variable from other variables
 struct Function {
-    arity: usize,
     name: String,
-    binding: Vec<Term>
+    args: Vec<Term>,
 }
 
 enum Quantifier {
@@ -52,4 +103,9 @@ fn main() {
     let equivalent = "\u{2194}";
 
     println!("{for_all},{exists},{not},{and},{or},{implies},{equivalent}");
+
+    let x = var!("x");
+    let y = var!("y");
+    let p = pred!("P", x, y);
+    println!("{p}");
 }
